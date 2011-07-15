e40bda871646b9da7e1ad8f407905f33eeef8023
Michael Bleigh <michael@intridea.com>
2011-07-15 17:10:26 -0500

    Adds basic twitter stream.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.mobile
+//= require jquery.tweet
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Partay::Application.routes.draw do
   match '/auth/:provider/callback', to: 'sessions#create'
   match '/sign_in', to: 'sessions#new', as: 'sign_in'
   match '/sign_out', to: 'sessions#destroy', as: 'sign_out'
+
+  match '/:id', to: 'pages#show', as: 'page'
 end
