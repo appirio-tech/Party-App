8742dc61863a02abf1236abe526629dffe444fc3
Michael Bleigh <michael@intridea.com>
2011-07-18 08:46:37 -0500

    Add production host config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,4 +51,5 @@ Partay::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => "partay.herokuapp.com" }  
 end
