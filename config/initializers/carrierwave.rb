7ee64b98903edcd13ab35b1fefe676ccbf845477
David Messinger <davemess34@gmail.com>
2011-08-10 12:16:42 -0400

    changed folder

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,5 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['S3_KEY'],       # required
     :aws_secret_access_key  => ENV['S3_SECRET'],       # required
   }
-  config.fog_directory = "partay_#{Rails.env}"
+  config.fog_directory = "/cloudspokes/partay_#{Rails.env}"
 end
